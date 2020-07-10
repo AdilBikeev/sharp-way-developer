2ff70c5603b7988dba3c4c24f9aa1a9822c1a437
AdCoder <bikeev1999@mail.ru>
2020-07-10 18:46:49 +0300

    #2 Типы данных / Конкатенация / Интерполяция строк

--- FirstProjectConsole/FirstProjectConsole/Program.cs
+++ FirstProjectConsole/FirstProjectConsole/Program.cs
@@ -6,7 +6,19 @@ namespace FirstProjectConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // {тип данных для хранения} {имя переменной};
+            // {тип данных для хранения} {имя переменной} = {значение переменной в соответствии её типы};
+
+            const string somthing = "RTU MIREA";
+            const string msg = "Hello";
+
+            Console.WriteLine(msg);
+            Console.WriteLine(msg + somthing);
+            Console.WriteLine(msg);
+            Console.WriteLine(msg);
+            Console.WriteLine($"{msg} {somthing}");
+            //Console.WriteLine("{msg} {somthing}");
+            //Console.WriteLine("Hello  RTU MIREA");
         }
     }
 }

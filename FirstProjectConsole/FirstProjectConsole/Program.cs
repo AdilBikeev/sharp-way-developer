936c8d8ca41a8d28c3e5a384e6a2f95e79c106df
AdCoder <bikeev1999@mail.ru>
2020-07-17 23:01:52 +0300

    #3 Потоковый ввод в C# / Read / ReadLine / ReadKey

--- FirstProjectConsole/FirstProjectConsole/Program.cs
+++ FirstProjectConsole/FirstProjectConsole/Program.cs
@@ -6,19 +6,13 @@ namespace FirstProjectConsole
     {
         static void Main(string[] args)
         {
-            // {тип данных для хранения} {имя переменной};
-            // {тип данных для хранения} {имя переменной} = {значение переменной в соответствии её типы};
+            Console.Write("Введите символ: ");
+            char symbol = Console.ReadKey().KeyChar;
+            Console.WriteLine("\n" + symbol);
 
-            const string somthing = "RTU MIREA";
-            const string msg = "Hello";
-
-            Console.WriteLine(msg);
-            Console.WriteLine(msg + somthing);
-            Console.WriteLine(msg);
-            Console.WriteLine(msg);
-            Console.WriteLine($"{msg} {somthing}");
-            //Console.WriteLine("{msg} {somthing}");
-            //Console.WriteLine("Hello  RTU MIREA");
+            Console.Write("Введите строку: ");
+            string messsage = Console.ReadLine();
+            Console.WriteLine($"Вы ввели строку: {messsage}");
         }
     }
 }

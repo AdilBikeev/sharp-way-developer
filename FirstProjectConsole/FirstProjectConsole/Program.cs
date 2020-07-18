317d10ae12bc87d29f0bea64308ad44921d875cc
AdilBikeev <bikeev1999@mail.ru>
2020-07-19 01:09:25 +0300

    #4 Блоки кода / Parse / TryParse

--- FirstProjectConsole/FirstProjectConsole/Program.cs
+++ FirstProjectConsole/FirstProjectConsole/Program.cs
@@ -6,6 +6,11 @@ namespace FirstProjectConsole
     {
         static void Main(string[] args)
         {
+            Console.Write("Пожалуйста введите число: ");
+            int first;
+            int.TryParse(Console.ReadLine(), out first);
+            Console.WriteLine("Вы ввели число: " + first);
+
             Console.Write("Введите символ: ");
             char symbol = Console.ReadKey().KeyChar;
             Console.WriteLine("\n" + symbol);
